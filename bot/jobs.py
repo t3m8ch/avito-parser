d03c8268e19cd6dad730658ff265fabb2ae8af0c
Kudyakov Artem <t3m8ch@yandex.ru>
2021-07-04 22:53:30 +0400

    refactor: Removing unnecessary 'if'

--- bot/jobs.py
+++ bot/jobs.py
@@ -21,6 +21,5 @@ async def send_new_ads_job(bot: Bot,
     ads = parser.parse(html)
     ads = await ad_repo.add_ads(ads)
 
-    if ads:
-        for ad in ads:
-            await bot.send_message(chat_id, str(ad))
+    for ad in ads:
+        await bot.send_message(chat_id, str(ad))
