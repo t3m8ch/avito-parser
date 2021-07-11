3647de68ddfc70b016d2b020d70ab138c1a9c2c8
Kudyakov Artem <t3m8ch@yandex.ru>
2021-07-11 19:37:41 +0400

    feat: Create GoogleSheetsService

---
+++ bot/services/google_sheets.py
@@ -0,0 +1,32 @@
+from typing import Iterable
+
+from bot.misc.models import AdModel
+from gspread_asyncio import AsyncioGspreadClientManager
+
+
+class GoogleSheetsService:
+    def __init__(self, spreadsheet_client_manager: AsyncioGspreadClientManager):
+        self._spreadsheet_client_manager = spreadsheet_client_manager
+
+    async def get_url_to_ads_spreadsheet(self, ads: Iterable[AdModel]) -> str:
+        spreadsheet_client = await self._spreadsheet_client_manager.authorize()
+
+        spreadsheet = await spreadsheet_client.create("Ваши подписки")
+        spreadsheet_url = f"https://docs.google.com/spreadsheets/d/{spreadsheet.id}"
+
+        await spreadsheet_client.insert_permission(
+            file_id=spreadsheet.id,
+            value=None,
+            perm_type="anyone",
+            role="writer"
+        )
+
+        worksheet = await spreadsheet.get_worksheet(0)
+        
+        await worksheet.insert_row(["Загаловок", "Цена", "Адрес"])
+        await worksheet.insert_rows([
+            [ad.title, ad.price or "", ad.url]
+            for ad in ads
+        ])
+        
+        return spreadsheet_url
